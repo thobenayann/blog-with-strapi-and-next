dbeced0d20a053ff25aece26470f5385d2b9ad00
thobenayann <63640040+thobenayann@users.noreply.github.com>
2021-09-13 08:11:28 +0200

    first commit

---
+++ my-strapi-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '78150650d0b3dc9afe19df256d43a302'),
+    },
+  },
+});
---
+++ my-strapi-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be6d72bd-db83-4473-b875-56260693cd84'
+};
